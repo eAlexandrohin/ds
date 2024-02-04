e6cc6236d5eaeebb829a55ac3c258f7c320a63a7
ealexandrohin <ealexandrohin@gmail.com>
2024-02-04 14:53:14 +0300

    takenDate

--- ds.js
+++ ds.js
@@ -1,15 +1,22 @@
-const fs = require('fs'), path = 'C:/Users/Alex/Desktop', now = new Date().toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit', year: 'numeric'}).replaceAll('/', '.');
-fs.mkdir(`C:/Users/Alex/Desktop/f/[${now}]`, null, (err) => {
-	if (err) {
-		if (err.code === 'EEXIST') {clear()}
-		else {console.error(err)};
-	} else {clear()};
-});
+const 
+	fs = require(`fs`),
+	FROM = `C:/Users/Alex/Desktop`,
+	TO = `C:/Users/Alex/Pictures/f`,
+	regex = /^\d+\-\d+\-\d+\_\d+\-(\d+|\d+\_\d+)\.\w+$/g
+;
 
-function clear() {
-	fs.readdir(path, null, (err, files) => {
-		files.forEach((p) => {
-			/^\d+\-\d+\-\d+\_\d+\-(\d+|\d+\_\d+)\.\w+$|^IMG\_\d+\_\d+\.\w+$|^DSC\_\d+\.\w+$/g.test(p) ? fs.renameSync(`${path}/${p}`, `${path}/f/[${now}]/${p}`) : null;
-		});
+if (!fs.existsSync(TO)) fs.mkdirSync(TO);
+
+fs.readdir(FROM, null, (err, files) => {
+	files.forEach((path) => {
+		if (regex.test(path)) {
+			const FOLDER = `${TO}/[${path.split(`_`)[0].split(`-`).reverse().join(`.`)}]`;
+
+			fs.mkdir(FOLDER, null, (err) => {
+				if (err && err.code != `EEXIST`) console.error;
+
+				fs.renameSync(`${FROM}/${path}`, `${FOLDER}/${path}`)
+			});
+		};
 	});
-};
+});
