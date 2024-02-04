3eba218eef41ec7b7169ba432fe859d434efb770
ealexandrohin <ealexandrohin@gmail.com>
2024-02-04 14:52:19 +0300

    newDate

--- ds.js
+++ ds.js
@@ -1,10 +1,9 @@
-const fs = require('fs'), path = 'C:/Users/Alex/Desktop', now = new Date().toLocaleDateString().replaceAll(/\//g, '.');
-fs.mkdir(`C:/Users/Alex/Desktop/f/[${now}]`, (err) => {
-	switch (true) {
-		case (err.code === 'EEXIST'): clear(); break;
-		case (err): console.error(err); break;
-		default: clear();
-	};
+const fs = require('fs'), path = 'C:/Users/Alex/Desktop', now = new Date().toLocaleDateString('en-GB', {day: '2-digit', month: '2-digit', year: 'numeric'}).replaceAll('/', '.');
+fs.mkdir(`C:/Users/Alex/Desktop/f/[${now}]`, null, (err) => {
+	if (err) {
+		if (err.code === 'EEXIST') {clear()}
+		else {console.error(err)};
+	} else {clear()};
 });
 
 function clear() {
